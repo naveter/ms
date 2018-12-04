a80aa33ea9894ace53cb52d005ce43e4034c0058
bass <onlinekniga@mail.ru>
2018-12-04 15:50:41 +0300

    Before modification

--- main/src/main/java/com/kaviddiss/streamkafka/StreamKafkaApplication.java
+++ main/src/main/java/com/kaviddiss/streamkafka/StreamKafkaApplication.java
@@ -10,11 +10,5 @@ public class StreamKafkaApplication {
     public static void main(String[] args) {
         SpringApplication.run(StreamKafkaApplication.class, args);
 
-        Scanner reader = new Scanner(System.in);  // Reading from System.in
-        System.out.println("Enter a number: ");
-        int n = reader.nextInt();
-        reader.close();
-
-        System.out.println(String.valueOf(n));
     }
 }
--- main/src/main/java/com/kaviddiss/streamkafka/swing/MainJFrame.java
+++ main/src/main/java/com/kaviddiss/streamkafka/swing/MainJFrame.java
@@ -3,18 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package swingform;
+package com.kaviddiss.streamkafka.swing;
 
 /**
  *
  * @author bass
  */
-public class NewJFrame extends javax.swing.JFrame {
+public class MainJFrame extends javax.swing.JFrame {
 
     /**
      * Creates new form NewJFrame
      */
-    public NewJFrame() {
+    public MainJFrame() {
         initComponents();
     }
 
@@ -426,26 +426,27 @@ public class NewJFrame extends javax.swing.JFrame {
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
+                if ("Windows".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new NewJFrame().setVisible(true);
+                new MainJFrame().setVisible(true);
             }
         });
     }
